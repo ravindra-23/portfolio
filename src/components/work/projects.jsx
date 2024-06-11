4bf0ad858ca048b047598c91311fd2bf4423a69c
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2024-06-11 14:53:34 +0100

    add projects data

---
+++ src/components/work/projects.jsx
@@ -0,0 +1,156 @@
+import Notion from "../../assets/notion.png";
+import Taskai from "../../assets/task-ai.png";
+import AiImage from "../../assets/ai-image.png";
+import Ecommerce from "../../assets/ecommerce.png";
+import Netflix from "../../assets/netflix-clone.png";
+import Pizzapp from "../../assets/pizza-app.png";
+import Evogym from "../../assets/evogym.png";
+import Crypto from "../../assets/crpto-tracker.png";
+import Notes from "../../assets/notes-app.png";
+import Restaurant from "../../assets/restaurant.png";
+import Cosmetics from "../../assets/cosmetics.png";
+import Hmo from "../../assets/hmo-website.png";
+import Powerforce from "../../assets/power-force.png";
+import Sharpener from "../../assets/sharpener.png";
+import Woodelement from "../../assets/wood-element.png";
+import Acros from "../../assets/acros.png";
+import Bluelotus from "../../assets/bluelotus.png";
+
+export const projects = [
+  {
+    id: 1,
+    title: "Notion Clone",
+    image: Notion,
+    value: "web apps",
+    tech: "Next js, React js, Convex DB, Clerk, Tailwind CSS",
+    url: "https://note-taking-app-next.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Task AI",
+    image: Taskai,
+    value: "web apps",
+    tech: "React js, Redux, Tailwind CSS, MongoDB, Openai API",
+    url: "https://task-ai.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "AI Image Generator",
+    image: AiImage,
+    value: "web apps",
+    tech: "React js, Tailwind CSS, MongoDB, Openai API",
+    url: "https://ai-image-mern.netlify.app/",
+  },
+  {
+    id: 4,
+    title: "Ecommerce Website",
+    image: Ecommerce,
+    value: "web apps",
+    tech: "React js, Material UI",
+    url: "https://myntra-react-ecommerce.netlify.app/",
+  },
+  {
+    id: 5,
+    title: "Crypto Tracker",
+    image: Crypto,
+    value: "web apps",
+    tech: "React js, Redux, Material UI, Coingeko API",
+    url: "https://crypto-tracker-react-redux.netlify.app/",
+  },
+  {
+    id: 6,
+    title: "Netflix Clone",
+    image: Netflix,
+    value: "web apps",
+    tech: "React js, Firebase, Styled Components",
+    url: "https://netflix-clone-react2308.netlify.app/",
+  },
+  {
+    id: 7,
+    title: "Pizza Delivery",
+    image: Pizzapp,
+    value: "web apps",
+    tech: "React js, Material UI",
+    url: "https://pizza-app-react2308.netlify.app/",
+  },
+  {
+    id: 8,
+    title: "Notes",
+    image: Notes,
+    value: "web apps",
+    tech: "React js, Firebase, Styled Components",
+    url: "https://notes-app-javascript2308.netlify.app/",
+  },
+  {
+    id: 11,
+    title: "Cosmetics",
+    image: Cosmetics,
+    value: "websites",
+    tech: "React js, Tailwind CSS, Framer",
+    url: "https://cosmetics-co.netlify.app/",
+  },
+  {
+    id: 9,
+    title: "Sushi Restaurant",
+    image: Restaurant,
+    value: "websites",
+    tech: "HTML5, CSS3, JavaScript",
+    url: "https://sushi-restaurant-page.netlify.app/",
+  },
+  {
+    id: 10,
+    title: "Evogym",
+    image: Evogym,
+    value: "websites",
+    tech: "React js, Tailwind CSS, Typescript, Framer",
+    url: "https://evogym-fit.netlify.app/",
+  },
+  {
+    id: 12,
+    title: "HMO Designers",
+    image: Hmo,
+    value: "wordpress",
+    tech: "Wordpress, Elementor, JavaScript",
+    url: "https://hmodesigners.co.uk/",
+  },
+  {
+    id: 13,
+    title: "Power Force",
+    image: Powerforce,
+    value: "wordpress",
+    tech: "Wordpress, Elementor, JavaScript",
+    url: "https://pfrenewables.co.uk/",
+  },
+  {
+    id: 14,
+    title: "The Sharpener",
+    image: Sharpener,
+    value: "wordpress",
+    tech: "Wordpress, Elementor, JavaScript",
+    url: "https://thesharpener.co/",
+  },
+  {
+    id: 15,
+    title: "The Wood Element",
+    image: Woodelement,
+    value: "wordpress",
+    tech: "Wordpress, Elementor, JavaScript",
+    url: "https://thewoodelement.in/",
+  },
+  {
+    id: 16,
+    title: "Acros",
+    image: Acros,
+    value: "wordpress",
+    tech: "Wordpress, Elementor, JavaScript",
+    url: "https://acros.thesharpener.co/",
+  },
+  {
+    id: 17,
+    title: "Blue Lotus",
+    image: Bluelotus,
+    value: "wordpress",
+    tech: "Wordpress, Elementor, JavaScript",
+    url: "https://bluelotus.thesharpener.co/",
+  },
+];
